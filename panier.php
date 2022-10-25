537441a16d6d12cbd05520af59f498ef137a3d6c
Alexie <alexie.grosbois@etudiant.univ-rennes1.fr>
2022-10-25 11:02:09 +0200

    ajout fonts et images

---
+++ panier.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div class="produit_panier">
+            <img class="image" src="/images/100_vannes/mortbihan/mortbihan_face.png" width="150px" alt="photo face pull">
+            <div class="descp_produit_panier">
+                <p class="titre_produit_panier">T-shirt "Mort-bihan"</p>
+                <p class="font--text">Vêtements - Taille L</p>
+                <p class="font--text">75€</p>
+            </div>
+        </div>
+        <hr class="ligne_sep" color="#DD6E42">
+        <div class="produit_panier">
+            <img class="image" src="/images/100_vannes/regardbzh1/regardbzh3_face.png" width="150px" alt="photo face pull">
+            <div class="descp_produit_panier">
+                <p class="titre_produit_panier">T-shirt "Regard Breizh"</p>
+                <p class="font--text">Vêtements - Taille L</p>
+                <p class="font--text">75€</p>
+            </div>
+        </div>
+        <hr class="ligne_sep" color="#DD6E42">
+        <div class="produit_panier">
+            <img class="image" src="/images/100_vannes/vie_en_roz/vie_en_roz_1.png" width="150px" alt="photo sac face">
+            <div class="descp_produit_panier">
+                <p class="titre_produit_panier">Sac à main "Vie en ROZ</p>
+                <p class="font--text">Catégorie - Taille Unique</p>
+                <p class="font--text">75€</p>
+            </div>
+        </div>
+    </div>
+    <div class="rect_detail_commande">
+        <h2 class="titre_produit_panier">Détail de la commande</h2>
+        <div class="text--detail">
+            <p class="font--text">3x T-shirt "Mort-bihan"</p>
+            <p class="font--text">3 x 75€</p>
+        </div>
+        <hr class="dash_ligne" width="200px">
+        <div>
+            <p class="font--text">Frais de livraison</p>
+        </div>
+        <hr class="dash_ligne" width="200px">
+        <div>
+            <p class="font--text">Prix HT</p>
+            <p class="font--text">180€</p>
+        </div>
+        <div>
+            <p class="font--text">TVA (20%)</p>
+            <p class="font--text">45€</p>
+        </div>
+        <hr color="#DD6E42" width="200px">
+        <div>
+            <p class="font--text">Prix Total</p>
+            <p class="font--text">225€</p>
+        </div>
+        <div>
+            <button class="btn--commander btn--text">Commander</button>
+        </div>
+    </div>
+</body>
+</html>
