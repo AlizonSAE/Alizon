88d636308b16b555324e360fe08e8f8f23d80ec9
Alexie <alexie.grosbois@etudiant.univ-rennes1.fr>
2022-10-25 16:09:27 +0200

    avancement de la partie détail du panier

--- panier.php
+++ panier.php
@@ -9,59 +9,75 @@
 </head>
 <body>
     <div>
-        <div class="produit_panier">
+        <div class="div__produit--panier">
             <img class="image" src="/images/100_vannes/mortbihan/mortbihan_face.png" width="150px" alt="photo face pull">
-            <div class="descp_produit_panier">
+            <div class="div__produit--description">
                 <p class="titre_produit_panier">T-shirt "Mort-bihan"</p>
                 <p class="font--text">Vêtements - Taille L</p>
                 <p class="font--text">75€</p>
             </div>
         </div>
-        <hr class="ligne_sep" color="#DD6E42">
-        <div class="produit_panier">
+        <hr class="hr__ligne--pleine" color="#DD6E42">
+        <div class="div__produit--panier">
             <img class="image" src="/images/100_vannes/regardbzh1/regardbzh3_face.png" width="150px" alt="photo face pull">
-            <div class="descp_produit_panier">
+            <div class="div__produit--description">
                 <p class="titre_produit_panier">T-shirt "Regard Breizh"</p>
                 <p class="font--text">Vêtements - Taille L</p>
                 <p class="font--text">75€</p>
             </div>
         </div>
-        <hr class="ligne_sep" color="#DD6E42">
-        <div class="produit_panier">
+        <hr class="hr__ligne--pleine" color="#DD6E42">
+        <div class="div__produit--panier">
             <img class="image" src="/images/100_vannes/vie_en_roz/vie_en_roz_1.png" width="150px" alt="photo sac face">
-            <div class="descp_produit_panier">
+            <div class="div__produit--description">
                 <p class="titre_produit_panier">Sac à main "Vie en ROZ</p>
                 <p class="font--text">Catégorie - Taille Unique</p>
                 <p class="font--text">75€</p>
             </div>
         </div>
     </div>
-    <div class="rect_detail_commande">
+    <div class="div__detail__commande">
         <h2 class="titre_produit_panier">Détail de la commande</h2>
-        <div class="text--detail">
-            <p class="font--text">3x T-shirt "Mort-bihan"</p>
-            <p class="font--text">3 x 75€</p>
+        <div class="text__detail">
+            <div class="one">
+                <p class="font--text">3x T-shirt "Mort-bihan"</p>
+            </div>
+            <div class="two">
+                <p class="font--text p__detail">3 x 75€</p>
+            </div>
         </div>
-        <hr class="dash_ligne" width="200px">
-        <div>
-            <p class="font--text">Frais de livraison</p>
+        <hr class="hr__ligne--dash" width="200px">
+        <div class="text__detail">
+            <div class="three">
+                <p class="font--text">Frais de livraison</p>
+            </div>
         </div>
-        <hr class="dash_ligne" width="200px">
-        <div>
-            <p class="font--text">Prix HT</p>
-            <p class="font--text">180€</p>
-        </div>
-        <div>
-            <p class="font--text">TVA (20%)</p>
-            <p class="font--text">45€</p>
+        <hr class="hr__ligne--dash" width="200px">
+        <div class="text__detail">
+            <div class="four">
+                <p class="font--text">Prix HT</p>
+            </div>
+            <div class="five">
+                <p class="font--text p__detail">180€</p>
+            </div>
+            <div class="six">
+                <p class="font--text">TVA (20%)</p>
+            </div>
+            <div class="seven">
+                <p class="font--text p__detail">45€</p>
+            </div>
         </div>
         <hr color="#DD6E42" width="200px">
-        <div>
-            <p class="font--text">Prix Total</p>
-            <p class="font--text">225€</p>
+        <div class="text__detail">
+            <div class="eight">
+                <p class="font--text">Prix Total</p>
+            </div>
+            <div class="nine">
+                <p class="font--text p__detail">225€</p>
+            </div>
         </div>
-        <div>
-            <button class="btn--commander btn--text">Commander</button>
+        <div class="div__detail--bouton">
+            <button class="btn__bouton btn__text">Commander</button>
         </div>
     </div>
 </body>
