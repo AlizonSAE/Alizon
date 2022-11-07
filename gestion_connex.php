5c56fe993f85405f93cbd5ff22fdbc46d86f6cd4
OnWix <81866454+OnWix@users.noreply.github.com>
2022-11-07 17:37:51 +0100

    Create gestion_connex.php

---
+++ gestion_connex.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+
+    // CONNEXION A LA BDD
+    include("../ressources/databases/connect_params.php");
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+
+    $email = $_POST['email'];
+
+    
+    $stmt = $dbh->query("SELECT passwd FROM alizon._client WHERE email = '$email'");
+    $stmt->execute();
+    $user = $stmt->fetch();
+
+
+    if (!password_verify($_POST['mot_de_passe'], $user['passwd'])){
+        header('Location: connexion.php?mdpinc=\'Mot de passe incorrect ou utilisateur inconnu, veuillez réessayer\'');
+    } else {
+        header('Location: index.php');
+    }
+
+
+
+?>
