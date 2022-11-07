2aaeb107fa5b8b167c0dc045f21f88a0b1697d91
OnWix <81866454+OnWix@users.noreply.github.com>
2022-11-07 17:36:35 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,2 +1,29 @@
-<?php
+<?php 
+    include("headers/tete.php");
+    // Le titre de la page entre els deux includes
+    echo "Alizon - Connexion";
 
+    include("headers/internaute.php");
+?>
+
+    <form id="inscription" method="POST" action="gestion_connex.php" class="inscription_connection">
+        <h2>Se connecter</h2>
+        <p class = 'error'>
+        <?php
+            if (!empty($_GET['mdpinc'])) {
+                echo $_GET['mdpinc'];
+            }
+        ?>
+        </p>
+        <label>E-mail<strong>*</strong></label><br>
+        <input type="email" name="email" placeholder="exemple@domaine.com" required><br>
+        
+        <label>Mot de passe<strong>*</strong></label><br>
+        <input type="password" name="mot_de_passe" placeholder="mot de passe" required><br>
+
+        <input type="submit" value="Se connecter" class="btn_inscr">
+        <p>Nouveau sur le site ? <a href="inscription.php">Inscrivez-vous !</a></p>
+    </form>
+<?php 
+    include("footers/internaute.php");
+?>
